19c800e3c03ac4996f26ebfbbbc1be8da37f1c6f
Dennis <ostermann3k@googlemail.com>
2025-07-14 10:09:04 +0200

    Create inital Avalonia Project

---
+++ MouseJigglerAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MouseJigglerAvalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ MouseJigglerAvalonia/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace MouseJigglerAvalonia.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}

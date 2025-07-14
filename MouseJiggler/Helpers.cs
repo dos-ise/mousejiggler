8cb672a527d1154bd74f65ccfbef4fce5fc073f3
Dennis <ostermann3k@googlemail.com>
2025-07-14 10:02:57 +0200

    Use DllImport for Jiggle in Helpers

--- MouseJiggler/Helpers.cs
+++ MouseJiggler/Helpers.cs
@@ -4,6 +4,7 @@
 // 
 // Created by: Alistair J R Young (avatar) at 2021/01/20 7:40 PM.
 // Updates by: Dimitris Panokostas (midwan)
+// Updates by: Dennis
 
 #endregion
 
@@ -12,8 +13,6 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using Windows.Win32;
-using Windows.Win32.UI.Input.KeyboardAndMouse;
 
 #endregion
 
@@ -30,40 +29,61 @@ internal static class Helpers
 
     #endregion Console management
 
-    #region Jiggling
 
-    /// <summary>
-    ///     Jiggle the mouse; i.e., fake a mouse movement event.
-    /// </summary>
-    /// <param name="delta">The mouse will be moved by delta pixels along both X and Y.</param>
-    internal static void Jiggle(int delta)
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct INPUT
     {
-        var inp = new INPUT
-        {
-          type = INPUT_TYPE.INPUT_MOUSE,
-          Anonymous = new INPUT._Anonymous_e__Union
-          {
-            mi = new MOUSEINPUT
-            {
-              dx = delta,
-              dy = delta,
-              mouseData = 0,
-              dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE,
-              time = 0,
-              dwExtraInfo = 0
-            }
-          }
-        };
-    
-        var returnValue = PInvoke.SendInput(new ReadOnlySpan<INPUT>(in inp), Marshal.SizeOf<INPUT>());
-
-        if (returnValue == 1) return;
-        var errorCode = Marshal.GetLastWin32Error();
-
-        Debugger.Log(1,
-            "Jiggle",
-            $"failed to insert event to input stream; retval={returnValue}, errcode=0x{errorCode:x8}\n");
+        public uint type;
+        public MOUSEINPUT mi;
     }
 
-    #endregion Jiggling
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MOUSEINPUT
+    {
+        public int dx;
+        public int dy;
+        public uint mouseData;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    internal static class INPUT_TYPE
+    {
+        public const uint INPUT_MOUSE = 0;
+    }
+
+    internal static class MOUSE_EVENT_FLAGS
+    {
+        public const uint MOUSEEVENTF_MOVE = 0x0001;
+    }
+
+    internal static void Jiggle(int delta)
+    {
+        var input = new INPUT
+        {
+            type = INPUT_TYPE.INPUT_MOUSE,
+            mi = new MOUSEINPUT
+            {
+                dx = delta,
+                dy = delta,
+                mouseData = 0,
+                dwFlags = MOUSE_EVENT_FLAGS.MOUSEEVENTF_MOVE,
+                time = 0,
+                dwExtraInfo = IntPtr.Zero
+            }
+        };
+
+        var inputs = new INPUT[] { input };
+        var result = SendInput((uint)inputs.Length, inputs, Marshal.SizeOf<INPUT>());
+
+        if (result == 0)
+        {
+            var errorCode = Marshal.GetLastWin32Error();
+            Debugger.Log(1, "Jiggle", $"SendInput failed: error code 0x{errorCode:X8}\n");
+        }
+    }
 }
